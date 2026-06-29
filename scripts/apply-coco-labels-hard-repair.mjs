4b6f7119c0aea38311cd5297d7848c3ed062e097
kimtaejoon01 <125454302+kimtaejoon01@users.noreply.github.com>
2026-06-30 06:04:52 +0900

    Repair repeated COCO ALL_LABELS replacement

---
+++ scripts/apply-coco-labels-hard-repair.mjs
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import fs from 'node:fs'
+
+const file = 'public/static/coco.js'
+const before = fs.readFileSync(file, 'utf8').replace(/\r\n/g, '\n')
+let s = before
+
+s = s.replace(/ALL_(?:ALL_)+LABELS/g, 'ALL_LABELS')
+s = s.replace("import { LABELS, getSupercategory } from './labels.js'", "import { ALL_LABELS, getSupercategory } from './labels.js'")
+s = s.replace(/\bLABELS\s*\n\s*\.filter/g, 'ALL_LABELS\n    .filter')
+s = s.replace(/\bLABELS\.indexOf\(/g, 'ALL_LABELS.indexOf(')
+
+if (s !== before) {
+  fs.writeFileSync(file, s)
+  console.log('PATCH COCO ALL_LABELS hard repair')
+} else {
+  console.log('OK COCO ALL_LABELS hard repair already patched')
+}
